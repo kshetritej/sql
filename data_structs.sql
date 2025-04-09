eceaafb556a4a4f6413905a75e75c05a14107e07
kshetritej <tejghartikshetri@gmail.com>
2025-04-09 14:15:22 +0545

    add: data struct describe query file

---
+++ data_structs.sql
@@ -0,0 +1,11 @@
+DESCRIBE Author;
+DESCRIBE Book;
+DESCRIBE BookAuthor;
+DESCRIBE BookCopy;
+DESCRIBE User;
+DESCRIBE Loan;
+DESCRIBE Reservation;
+DESCRIBE FineRate;
+DESCRIBE Room;
+DESCRIBE RoomBooking;
+DESCRIBE LoanLog;
